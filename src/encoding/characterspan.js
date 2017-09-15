e6af50bb69c52311e0a2532e4e6f61979b75dafa
Cyril Vlaminck <vlaminck.cyril@gmail.com>
2017-09-15 07:29:27 +0200

    Fixed factory method

--- src/encoding/characterspan.js
+++ src/encoding/characterspan.js
@@ -20,7 +20,7 @@ export default class CharacterSpan {
      * @return {CharacterSpan} a new span.
      */
     static newRange(startCharacter, endCharacter) {
-
+        return new CharacterSpan(startCharacter, endCharacter);
     }
 
     /**
@@ -29,7 +29,7 @@ export default class CharacterSpan {
      * @return {CharacterSpan} a new span.
      */
     static newSingleCharacter(character) {
-
+        return new CharacterSpan(character, character);
     }
 
     /**
