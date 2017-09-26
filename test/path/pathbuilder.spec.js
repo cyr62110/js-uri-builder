a67e798eb60f6b6618f27fdede73d0a8fd377699
Cyril Vlaminck <vlaminck.cyril@gmail.com>
2017-09-26 07:56:50 +0200

    Improved test on appendEncodedPathSegment

--- test/path/pathbuilder.spec.js
+++ test/path/pathbuilder.spec.js
@@ -18,26 +18,30 @@ describe("PathBuilder", () => {
             let validator = new PathSegmentValidator();
             let encoding = UriEncoding.getDefault();
 
+            sinon.spy(validator, "validate");
+
             let path = new PathBuilder(null, encoding, validator)
                 .appendEncodedPathSegment("test")
                 .build();
 
+            expect(validator.validate.calledWith("test")).to.be.true;
             expect(path.pathSegmentCount).to.equal(1);
             expect(path.getEncodedPathSegment(0)).to.equal("test");
         });
 
         it("throws if malformed path segment", () => {
             let builder = new PathBuilder(null, UriEncoding.getDefault(), new PathSegmentValidator());
-            expect(() => {builder.appendEncodedPathSegment("test!")}).to.throw();
+            expect(() => {builder.appendEncodedPathSegment("Hello world")}).to.throw();
         });
     });
 
     describe("appendPathSegment", () => {
         it("to encode and append", () => {
-            let validator = sinon.mock(PathSegmentValidator);
+            let validator = new PathSegmentValidator();
             let encoding = UriEncoding.getDefault();
             let builder = new PathBuilder(null, encoding, validator);
 
+            sinon.stub(validator, "validate");
             sinon.stub(encoding, "encode").withArgs("test!").returns("test%21");
             sinon.spy(builder, "appendEncodedPathSegment");
 
@@ -45,8 +49,9 @@ describe("PathBuilder", () => {
 
             let path = builder.build();
             expect(path.pathSegmentCount).to.equal(1);
-            expect(paht.getEncodedPathSegment(0)).to.equal("test%21");
+            expect(path.getEncodedPathSegment(0)).to.equal("test%21");
             expect(encoding.encode.calledOnce).to.be.true;
+            expect(builder.appendEncodedPathSegment.calledWith("test%21")).to.be.true;
         });
     })
 });
